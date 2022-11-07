36231d34cab6c5783bfb4d949a919554fddb2804
Tatiana Zhuravleva <zhuravlini@gmail.com>
2022-11-07 19:54:11 +0300

    create test MaxService

---
+++ src/main/java/ru/netology/javaqa/MaxService.java
@@ -0,0 +1,13 @@
+package ru.netology.javaqa;
+
+public class MaxService {
+
+    public int max(int a, int b) {
+        if (a > b) {
+            return a;
+        } else {
+            return b;
+        }
+    }
+}
+
